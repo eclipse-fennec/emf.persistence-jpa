7841282bbe01eef0a9750d6e81cbdd2cfb7b07a7
Mark Hoffmann <m.hoffmann@datainmotion.com>
2026-03-12 13:03:15 +0100

    License header Eclipse Foundation
    Signed-off-by: Mark Hoffmann <m.hoffmann@datainmotion.com>

--- org.eclipse.fennec.persistence.eclipselink/src/org/eclipse/fennec/persistence/eclipselink/spi/PersistenceUnitConfigurator.java
+++ org.eclipse.fennec.persistence.eclipselink/src/org/eclipse/fennec/persistence/eclipselink/spi/PersistenceUnitConfigurator.java
@@ -1,5 +1,5 @@
-/*
-Ø * Copyright (c) 2024 Contributors to the Eclipse Foundation.
+/********************************************************************
+ * Copyright (c) 2026 Contributors to the Eclipse Foundation.
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -8,9 +8,8 @@
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
- *   SmartCity Jena - initial
- *   Stefan Bischof (bipolis.org) - initial
- */
+ *   Data In Motion Consulting - initial implementation
+ ********************************************************************/
 package org.eclipse.fennec.persistence.eclipselink.spi;
 
 import static java.util.Objects.isNull;
